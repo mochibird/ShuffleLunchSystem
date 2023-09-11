a56b45131c4a39a76c0b0562fbfb2d7c9f888823
mochibird <yutaro.mochi@gmail.com>
2023-09-11 05:57:32 +0000

    例外処置を実装

--- Application.php
+++ Application.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/core/Router.php');
+require_once(__DIR__ . '/core/HttpNotFoundException.php');
 require_once(__DIR__ . '/controller/ShuffleController.php');
 require_once(__DIR__ . '/controller/EmployeeController.php');
 class Application
@@ -14,15 +15,25 @@ class Application
 
     public function run()
     {
-        $params = $this->router->resolve($this->getPathInfo());
-        $controller = $params['controller'];
-        $action = $params['action'];
-        $this->runAction($controller, $action);
+        try {
+            $params = $this->router->resolve($this->getPathInfo());
+            if (!$params) {
+                throw new HttpNotFoundException();
+            }
+            $controller = $params['controller'];
+            $action = $params['action'];
+            $this->runAction($controller, $action);
+        } catch (HttpNotFoundException) {
+                $this->render404Page();
+        }
     }
 
     public function runAction(string $controller, string $action)
     {
         $controllerName = ucfirst($controller) . 'Controller';
+        if (!class_exists($controllerName)) {
+            throw new HttpNotFoundException();
+        }
         $controllerClass = new $controllerName();
         $controllerClass->run($action);
     }
@@ -42,4 +53,29 @@ class Application
         return $_SERVER['REQUEST_URI'];
     }
 
+    public function render404Page(): void
+    {
+        $content = <<<EOF
+        <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="昼食時間の社員のグループ分けを行うシステムです">
+    <title>Error 404</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="primary">
+            Not Found Page
+        </h1>
+    </div>
+</body>
+
+</html>
+EOF;
+        echo $content;
+    }
 }
