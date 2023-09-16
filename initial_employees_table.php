d2d40298b67533b7a3c40ddbd51eae158d2ca2e1
mochibird <yutaro.mochi@gmail.com>
2023-09-16 11:05:51 +0000

    add pdo

--- initial_employees_table.php
+++ initial_employees_table.php
@@ -1,28 +1,46 @@
 <?php
 
-$mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
-if ($mysqli->connect_error) {
-    throw new RuntimeException();
-}
+require_once(__DIR__ . '/vendor/autoload.php');
 
-function dropTableSql($mysqli)
+function dbConnect(): PDO
 {
-    $mysqli->query('DROP TABLE IF EXISTS employees');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
+    $dotenv->load();
+    $dbHost = $_ENV['DB_HOST'];
+    $dbUsername = $_ENV['DB_USERNAME'];
+    $dbPassword = $_ENV['DB_PASSWORD'];
+    $dbDatabase = $_ENV['DB_DATABASE'];
+    $dbSourceName = "mysql:host=$dbHost;dbname=$dbDatabase;charset=utf8mb4";
+    try {
+        $dbh = new PDO($dbSourceName, $dbUsername, $dbPassword, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
+    } catch (PDOException $e) {
+        echo 'DBエラー: ' . $e->getMessage();
+        exit();
+    }
+    return $dbh;
 }
 
-function createTableSql($mysqli)
+function dropTableSql(PDO $dbh)
+{
+    $dbh->query('DROP TABLE IF EXISTS employees');
+}
+
+function createTableSql(PDO $dbh)
 {
     $sql = <<<EOT
         CREATE TABLE employees(
             id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-            emp_no INTEGER NOT NULL,
-            name VARCHAR(50) NOT NULL,
+            emp_no INTEGER,
+            name VARCHAR(50),
             create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
         ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4
 EOT;
-    $mysqli->query($sql);
+    $dbh->query($sql);
 }
 
-dropTableSql($mysqli);
-createTableSql($mysqli);
-$mysqli->close();
+$dbh = dbConnect();
+dropTableSql($dbh);
+createTableSql($dbh);
+$dbh = null;
